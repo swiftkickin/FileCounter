e0f687101dacfb547ce3ae3804a0dcc4c187588f
Kevin Griffin <kevin@kevgriffin.com>
2018-07-31 15:12:06 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FileCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Environment.CurrentDirectory;
+            var totalFiles = GetFileCount(currentDirectory);
+
+            Console.WriteLine($"The directory {currentDirectory} contains {totalFiles} files.");
+        }
+
+        private static long GetFileCount(string currentDirectory)
+        {
+            var fileCount = System.IO.Directory.GetFiles(currentDirectory).LongLength;
+            var directories = System.IO.Directory.GetDirectories(currentDirectory);
+
+            foreach(var directory in directories){
+                fileCount += GetFileCount(directory);
+            }
+            return fileCount;
+        }
+    }
+}
